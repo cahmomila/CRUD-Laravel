98f9ef7e706dd8a6b8539946c2b004a2b496ca1c
camila souza <camilaes2405@hotmail.com>
2019-01-03 13:49:19 -0200

    crud-laravel

--- products/resources/views/products/create.blade.php
+++ products/resources/views/products/create.blade.php
@@ -3,8 +3,6 @@
 @section('content')
     @if(Auth::user())
         <div class="container">
-            <br>
-            <h2 class="text-center">Product</h2>
 
             @if(isset($errors))
                 @if ($errors->any())
@@ -17,50 +15,60 @@
                     </div>
                 @endif
             @endif
-            <form method="post" action="{{route('products.store')}}" enctype="multipart/form-data">
-                @csrf
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="title">Title:</label>
-                        <input type="text" class="form-control" value="{{ old('product.title') ?? '' }}"
-                               name="product[title]">
-                    </div>
-                </div>
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header"><h2 class="text-center">Create Product</h2></div>
 
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="description">Description:</label>
-                        <input type="text" class="form-control" value="{{ old('product.description') ?? '' }}"
-                               name="product[description]">
-                    </div>
-                </div>
+                        <div class="card-body">
+                            <form method="post" action="{{route('products.store')}}" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-group row pt-3">
+                                    <label for="title"
+                                           class="col-sm-4 col-form-label text-md-right">Title:</label>
 
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="image">Image:</label>
-                        <input type="file" name="product[image]">
+                                    <div class="col-md-4">
+                                        <input class="form-control" type="text"
+                                               name="product[title]" value="{{ old('product.title') ?? '' }}">
+                                    </div>
+                                </div>
+                                <div class="form-group row pt-3">
+                                    <label for="description"
+                                           class="col-sm-4 col-form-label text-md-right">Description:</label>
+                                    <div class="col-md-4">
+                                        <input class="form-control" type="text"
+                                               name="product[description]"
+                                               value="{{ old('product.description') ?? '' }}">
+                                    </div>
+                                </div>
+                                <div class="form-group row pt-3">
+                                    <label for="image"
+                                           class="col-sm-4 col-form-label text-md-right">Image:</label>
+                                    <div class="col-md-4">
+                                        <input type="file" name="product[image]">
+                                    </div>
+                                </div>
+                                <div class="form-group row pt-3">
+                                    <label for="price"
+                                           class="col-sm-4 col-form-label text-md-right">Price:</label>
+                                    <div class="col-md-4">
+                                        <input id="dinheiro" class="form-control" type="text"
+                                               name="product[price]" value="{{ old('product.price') ?? '' }}">
+                                    </div>
+                                </div>
+                                <div class="p-3 form-group row">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-success">
+                                            Create
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
+                    <a href="{{route('products.index')}}" class="mt-3 btn btn-dark"><i class="fa fa-arrow-left"></i> Go back</a>
                 </div>
-
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="price">Price:</label>
-                        <input type="text" class="form-control" id="dinheiro" value="{{ old('product.price') ?? '' }}"
-                               name="product[price]">
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4" style="margin-top:60px">
-                        <button type="submit" class="btn btn-success">Submit</button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     @else
         <div style="text-align: center">
@@ -68,7 +76,8 @@
                 block
             </i>
             <h1>You must be logged for access this page!</h1>
-            <a href="{{route('home')}}" class="btn btn-danger btn-lg active" role="button" aria-pressed="true">Go to
+            <a href="{{route('home')}}" class="btn btn-danger btn-lg active" role="button"
+               aria-pressed="true">Go to
                 login page</a>
         </div>
     @endif
--- products/resources/views/products/edit.blade.php
+++ products/resources/views/products/edit.blade.php
@@ -14,56 +14,67 @@
                     </div>
                 @endif
             @endif
-            <h2 class="text-center mt-3 mb-3">Edit Product</h2>
-            <form method="post" action="{{route('products.update', $id)}}" enctype="multipart/form-data">
-                @csrf
-                <input name="_method" type="hidden" value="PATCH">
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="title">Title:</label>
-                        <input type="text" class="form-control" name="product[title]"
-                               value="{{$product->title}}">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="description">Description</label>
-                        <input type="text" class="form-control" name="product[description]"
-                               value="{{$product->description}}">
-                    </div>
-                </div>
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header"><h2 class="text-center">Edit Product</h2></div>
 
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="image">Image:</label>
-                        <input type="file" name="product[image]" value="{{$product->image}}">
-                    </div>
-                </div>
+                        <div class="card-body">
+                            <form method="post" action="{{route('products.update', $id)}}"
+                                  enctype="multipart/form-data">
+                                @csrf
+                                <input name="_method" type="hidden" value="PATCH">
+                                <div class="form-group row pt-3">
+                                    <label for="title"
+                                           class="col-sm-4 col-form-label text-md-right">Title:</label>
 
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4">
-                        <label for="price">Price:</label>
-                        <input type="text" class="form-control" id="dinheiro" name="product[price]"
-                               value="{{$product->price}}">
+                                    <div class="col-md-4">
+                                        <input class="form-control" type="text"
+                                               name="product[title]" value="{{$product->title}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row pt-3">
+                                    <label for="description"
+                                           class="col-sm-4 col-form-label text-md-right">Description:</label>
+
+                                    <div class="col-md-4">
+                                        <input class="form-control" type="text"
+                                               name="product[description]" value="{{$product->description}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row pt-3">
+                                    <label for="image"
+                                           class="col-sm-4 col-form-label text-md-right">Image:</label>
+                                    <div class="col-md-4">
+                                        <input type="file" name="product[image]">
+                                    </div>
+                                </div>
+                                <div class="form-group row pt-3">
+                                    <label for="price"
+                                           class="col-sm-4 col-form-label text-md-right">Price:</label>
+                                    <div class="col-md-4">
+                                        <input id="dinheiro" class="form-control" type="text"
+                                               name="product[price]" value="{{$product->price}}">
+                                    </div>
+                                </div>
+                                <div class="p-3 form-group row">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-success">
+                                            Update
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
+                    <a href="{{route('products.index')}}" class="mt-3 btn btn-dark"><i class="fa fa-arrow-left"></i> Go back</a>
                 </div>
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="form-group col-md-4" style="margin-top:60px">
-                        <button type="submit" class="btn btn-success" style="margin-left:38px">Update</button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     @else
         <div style="text-align: center">
             <h1>You must be logged for access this page!</h1>
-            <a href="{{route('home')}}" class="btn btn-danger btn-lg active" role="button" aria-pressed="true">Go
-                to
+            <a href="{{route('home')}}" class="btn btn-danger btn-lg active" role="button" aria-pressed="true">Go to
                 login page</a>
         </div>
     @endif
